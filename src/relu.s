6938f9c10f0e77b20b31e6a0218532ecc1a17cbe
wx200010 <wx200010@gmail.com>
2024-11-16 13:02:09 +0800

    Update relu.s

--- src/relu.s
+++ src/relu.s
@@ -25,11 +25,22 @@
 relu:
     li t0, 1             
     blt a1, t0, error     
-    li t1, 0             
+    li t1, 0             # i = 0
+    mv t3, a0            # t3 is used to extract the element of array
 
-loop_start:
+loop_start:              # for(i = 0; i < a1, ++i)
+    lw t2, 0(t3)         # tmp = Input[i]
+    bgez t2, continue    # if Input[i] >= 0 , just skip
+    li t2, 0
+    sw t2, 0(t3)
+
+continue:
+    addi t3, t3, 1       # the array index ++
+    addi t1, t1, 1       # i++
+    bne t1, a1, loop_start     
+    li a0, 0
+    j exit
     # TODO: Add your own implementation
-
 error:
     li a0, 36          
     j exit          
