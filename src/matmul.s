bf8978da0c1e48f1c6d41097052fc5745788cb0c
wx200010 <wx200010@gmail.com>
2024-11-17 12:22:11 +0800

    Update matmul.s

--- src/matmul.s
+++ src/matmul.s
@@ -116,7 +116,23 @@ inner_loop_start:
     
 inner_loop_end:
     # TODO: Add your own implementation
+    mv t0, a2           # a2 is M0's column count
+    slli t0, t0, 2      # Because a element is 4bytes, we need to shift 4 * (column count) bytes 
+    add s3, s3, t0      # Move M0's element pointer to next row address
+    addi s0, s0, 1      # Outer loop counter + 1
+    j outer_loop_start  # Continue to calculate the next row
 
+outer_loop_end:
+    # Epilogue
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    lw s2, 12(sp)
+    lw s3, 16(sp)
+    lw s4, 20(sp)
+    lw s5, 24(sp)
+    addi sp, sp, 28
+    jr ra
 error:
     li a0, 38
     j exit
