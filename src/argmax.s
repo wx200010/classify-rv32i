d181c3e96cba6c4143a49374e5e6b5390b492225
wx200010 <wx200010@gmail.com>
2024-11-18 11:35:55 +0800

    Docs: the annotation of argmax.s

--- src/argmax.s
+++ src/argmax.s
@@ -25,21 +25,21 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)        # t0 is the maximum value
+    lw t0, 0(a0)        # t0 is the maximum value (initial as first element)
     addi t2, a0, 4      # t2 is index , which is used to extract the element from array  (arr[index])
-    li a0, 0            # a0 is the position of maximum value
-    li t3, 1            # t3 is the i  (i = 0)
-    beq a1, t3, final   # if len(arr) == 1, then it is done
-    
+    li a0, 0            # a0 is the position of maximum value (initial as first position)
+    beq a1, t3, final   # if len(arr) == 1, then it is done, go to return
+
+    li t3, 1            # t3 is the loop counter  (i = 1)
 loop_start:     
     # TODO: Add your own implementation
     lw t4, 0(t2)            # tmp = arr[index]
-    ble t4, t0, loop_check  # If arr[index] <= max, you don't need to update the maximum
+    ble t4, t0, loop_check  # If arr[index] <= max, continue to next loop
     mv a0, t3               # Update the position of maximum
     mv t0, t4               # Update the value of maximum
 loop_check:
     addi t3, t3, 1          # i++
-    addi t2, t2, 4          # index++
+    addi t2, t2, 4          # index++ (to extract the next element)
     bne t3, a1, loop_start  # Loop when i != len(arr)
 final:
     ret
